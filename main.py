15a115c1feb4322c9c1a7335de0ec709d234ec79
Israk Hossain Zidan <israkzidan450@gmail.com>
2026-04-03 11:57:54 -0400

    rename a variable

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def func2(data):
     print('Here is the data:')
     print(data)
 
-    choice = ''
+    option = ''
     while True:
-        choice = input('Select an option. 1-sum  2-min  3-max  4-quit')
-        if choice == '1':
+        option = input('Select an option. 1-sum  2-min  3-max  4-quit')
+        if option == '1':
             print('The sum is',sum(data))
-        elif choice == '2':
+        elif option == '2':
             print('Not supported yet')
-        elif choice == '3':
+        elif option == '3':
             print('Not supported yet')
-        elif choice == '4':
+        elif option == '4':
             print('Goodbye')
             return
 
